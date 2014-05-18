828e9a13169d0e66accbd49f5cb46ee7da54d1a2
Purushoth <purushothanbu@gmail.com>
2014-05-18 17:27:51 +0530

    Project Template

---
+++ FileToastr/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace FileToastr
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ FileToastr/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FileToastr.MvcApplication" Language="C#" %>
